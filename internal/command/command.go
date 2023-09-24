4421fcc472ea88e86707786637d6a8786caca696
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-09-24 11:23:08 +0200

    Define the behaviour of the command and add the `separator` flag.

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,12 +1,36 @@
 package command
 
 import (
+    "errors"
+
     "github.com/spf13/cobra"
+
+    "github.com/czuhajster/macspawn/internal/address"
+    "github.com/czuhajster/macspawn/internal/format"
 )
 
 var (
+    separator string
     rootCmd = &cobra.Command{
       Use:   "macspawn",
       Short: "MACSpawn is a MAC address generator.",
-    },
+      RunE: func(cmd *cobra.Command, args []string) error {
+          switch separator {
+          case ":", "-":
+          default:
+              return errors.New("Invalid separator.")
+          }
+          address := address.GenerateMACAddress()
+          format.PrintMAC(address, format.ColonFormat)
+          return nil
+      },
+    }
 )
+
+func init() {
+    rootCmd.Flags().StringVar(&separator, "separator", ":", "Separator of the address bytes.")
+}
+
+func Execute() error {
+    return rootCmd.Execute()
+}
