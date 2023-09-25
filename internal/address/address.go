eb3f57723cf970ff33d5f2a18f2cd69b6146ef30
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-09-25 17:57:04 +0200

    Make the GenerateIdentifier and GenerateNICSpecificBytes functions able to generate outputs of different lengths.

--- internal/address/address.go
+++ internal/address/address.go
@@ -1,72 +1,95 @@
 package address
 
 import (
-    "crypto/rand"
-    "errors"
+	"crypto/rand"
+	"errors"
 )
 
 type MACAddress [6]byte
 
 const (
-    localScopeBitmask byte = 2
-    universalScopeBitmask byte = 253
-    unicastBitmask byte = 254
-    multicastBitmask byte = 1
+	localScopeBitmask     byte = 2
+	universalScopeBitmask byte = 253
+	unicastBitmask        byte = 254
+	multicastBitmask      byte = 1
 )
 
-func GenerateIdentifier() *[3]byte {
-    var identifier [3]byte
-    _, err := rand.Read(identifier[:])
-    if err != nil {
-        panic(err)
+func GenerateIdentifier(length uint8) []byte {
+    remainingBits := 8 - (length % 8)
+	var identifier []byte
+	identifier = make([]byte, length + remainingBits)
+	_, err := rand.Read(identifier)
+	if err != nil {
+		panic(err)
+	}
+    if remainingBits > 0 {
+        bitmask := GenerateBitmask(remainingBits, false)
+        identifier[len(identifier)-1] &= bitmask
     }
-    return &identifier
+	return identifier
 }
 
-func GenerateNICSpecificBytes() *[3]byte {
-    var nicSpecificBytes [3]byte
-    _, err := rand.Read(nicSpecificBytes[:])
-    if err != nil {
-        panic(err)
+func GenerateNICSpecificBytes(length uint8) []byte {
+    remainingBits := 8 - (length % 8)
+	var nicSpecificBytes []byte
+	nicSpecificBytes = make([]byte, length + remainingBits)
+	_, err := rand.Read(nicSpecificBytes)
+	if err != nil {
+		panic(err)
+	}
+    if remainingBits > 0 {
+        bitmask := GenerateBitmask(remainingBits, true)
+        nicSpecificBytes[len(nicSpecificBytes)-1] &= bitmask
     }
-    return &nicSpecificBytes
+	return nicSpecificBytes
 }
 
 func GenerateMACAddress(local bool, individual bool) *MACAddress {
-    identifier := GenerateIdentifier()
-    if local == true {
-        identifier[0] |= localScopeBitmask
+    var identifierLength uint8 = 24
+	identifier := GenerateIdentifier(identifierLength)
+	if local == true {
+		identifier[0] |= localScopeBitmask
+	} else {
+		identifier[0] &= universalScopeBitmask
+	}
+	if individual == true {
+		identifier[0] &= unicastBitmask
+	} else {
+		identifier[0] |= multicastBitmask
+	}
+	nicSpecificBytes := GenerateNICSpecificBytes(48 - identifierLength)
+	address := MACAddress{identifier[0], identifier[1], identifier[2], nicSpecificBytes[0], nicSpecificBytes[1], nicSpecificBytes[2]}
+	return &address
+}
+
+func GenerateBitmask(shifts uint8, reverse bool) byte {
+    var bitmask byte = 255
+    if !reverse {
+        bitmask <<= shifts
     } else {
-        identifier[0] &= universalScopeBitmask
+        bitmask >>= shifts
     }
-    if individual == true {
-        identifier[0] &= unicastBitmask
-    } else {
-        identifier[0] |= multicastBitmask
-    }
-    nicSpecificBytes := GenerateNICSpecificBytes()
-    address := MACAddress{identifier[0], identifier[1], identifier[2], nicSpecificBytes[0], nicSpecificBytes[1], nicSpecificBytes[2]}
-    return &address
+    return bitmask
 }
 
 func CheckAddressType(addressType string) (bool, error) {
-    switch addressType {
-    case "individual", "i":
-        return true, nil
-    case "group", "g":
-        return false, nil
-    default:
-        return false, errors.New("Unrecognised address type.")
-    }
+	switch addressType {
+	case "individual", "i":
+		return true, nil
+	case "group", "g":
+		return false, nil
+	default:
+		return false, errors.New("Unrecognised address type.")
+	}
 }
 
 func CheckAddressScope(scope string) (bool, error) {
-    switch scope {
-    case "local", "l":
-        return true, nil
-    case "universal", "u":
-        return false, nil
-    default:
-        return false, errors.New("Unrecognised scope.")
-    }
+	switch scope {
+	case "local", "l":
+		return true, nil
+	case "universal", "u":
+		return false, nil
+	default:
+		return false, errors.New("Unrecognised scope.")
+	}
 }
