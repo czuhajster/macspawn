65cd6db05cf2066163648adeefbf3236a406e249
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-09-28 22:20:30 +0200

    Fix the code formatting in the validation.go file of the command package.

--- internal/command/validation.go
+++ internal/command/validation.go
@@ -10,10 +10,10 @@ import (
 // - XX:XX:XX:XX:X
 // where `X` is a hexadecimal symbol and `:` can be replaced either with `-` or
 // with the empty string.
-const identifierRegexp string = `^[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}(|([\-:]?[0-9a-fA-F]{1})|([\-:]?[0-9a-fA-F]{2}){2}[\-:]?[0-9a-fA-F]{1})$` 
+const identifierRegexp string = `^[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}(|([\-:]?[0-9a-fA-F]{1})|([\-:]?[0-9a-fA-F]{2}){2}[\-:]?[0-9a-fA-F]{1})$`
 
 func validateIdentifierFlag(identifier string) (bool, error) {
-    re, err := regexp.Compile(identifierRegexp)
+	re, err := regexp.Compile(identifierRegexp)
 	if err != nil {
 		return false, err
 	}
