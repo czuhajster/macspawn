dcc8fb04dc5100b4dcbf0cd1bc107beed718f99f
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-09-28 22:08:41 +0200

    Fix the validation regex for the identifier flag to also allow 28- and 36-bit identifiers.

--- internal/command/validation.go
+++ internal/command/validation.go
@@ -4,8 +4,16 @@ import (
 	"regexp"
 )
 
+// Matches one of the following:
+// - XX:XX:XX
+// - XX:XX:XX:X
+// - XX:XX:XX:XX:X
+// where `X` is a hexadecimal symbol and `:` can be replaced either with `-` or
+// with the empty string.
+const identifierRegexp string = `^[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}(|([\-:]?[0-9a-fA-F]{1})|([\-:]?[0-9a-fA-F]{2}){2}[\-:]?[0-9a-fA-F]{1})$` 
+
 func validateIdentifierFlag(identifier string) (bool, error) {
-	re, err := regexp.Compile(`^[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}[\-:]?[0-9a-fA-F]{2}`)
+    re, err := regexp.Compile(identifierRegexp)
 	if err != nil {
 		return false, err
 	}
